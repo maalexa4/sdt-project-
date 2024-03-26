d5d796fb61cdd041c9c9ebbfb91b61ab2cfd4c47
maalexa4 <maalexa4@asu.edu>
2024-03-25 23:41:02 -0700

    This project is a tool designed to simulate random events. It provides users with the ability to specify parameters for different types of events and generate simulated outcomes. The tool can be used for various purposes such as statistical analysis, decision-making simulations, and educational purposes.
    The project is implemented using the following
    methods and libraries:
    - Streamlit: For building the user interface and interactive components.
    - Python: For backend logic and simulation algorithms.
    - Plotly Express: For interactive data visualization.
    - NumPy: For numerical computing and array manipulation.
    - Python: For backend logic and simulation algorithms.
    git push

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # sdt-project-
+
+
+## Project Description:
+This project is a tool designed to simulate random events. It provides users with the ability to specify parameters for different types of events and generate simulated outcomes. The tool can be used for various purposes such as statistical analysis, decision-making simulations, and educational purposes.
+
+## Methods and Libraries Used:
+The project is implemented using the following methods and libraries:
+- Streamlit: For building the user interface and interactive components.
+- Python: For backend logic and simulation algorithms.
+- Plotly Express: For interactive data visualization.
+- NumPy: For numerical computing and array manipulation.
+- Python: For backend logic and simulation algorithms.
+
+## Instructions for Running the Project:
+To run this project on your local machine, follow these steps:
+
+1. Make sure you have Python installed on your machine. You can download it from [python.org](https://www.python.org/).
+
+2. Clone this repository to your local machine:
+https://github.com/maalexa4/sdt-project-.git
+
+
+3. Navigate to the project directory
+
+4. Install the required Python libraries using pip
+
+5. Run the Streamlit application
+
+6. Access the application in your web browser at `http://localhost:8502`.
+
+Feel free to explore the different features and options provided by the application.
+
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
+import numpy as np
 
 #creating header with checkbox
 st.header('Market of used cars.Original data')
@@ -9,11 +9,56 @@ st.write("""
 ##### Filter the data below to see the ads by manufacturer
 """)
 
-st.write("""
-##### this is  a new message
-""")
-df = pd.read_csv('vehicles_us(1).csv')
+
+df = pd.read_csv('vehicles_us (1).csv')
+
 df = df.drop(df.columns[0], axis=1)
 manufacturer_choise = df['model'].unique()
 
-st.selectbox('Select a model', manufacturer_choise)
+selected_menu = st.selectbox('Select a model', manufacturer_choise )
+
+min_year, max_year = int(df['model_year'].min()), int(df['model_year'].max())
+
+
+year_range = st.slider("choose years", value=(min_year, max_year), min_value=min_year,max_value= max_year)
+
+
+
+actual_range = list(range(year_range[0], year_range[1]+1))
+
+df_filtered = df[ (df.model == selected_menu) & (df.model_year.isin(list(actual_range)) )]
+
+df_filtered
+
+
+st.header('Price analysis')
+st.write("""
+###### Let's analyze what influences price the most. We will check how distribution of price varies depending on transmission, fuel or type and condition
+""")
+
+list_for_hist = ('transmission','fuel','type','condition')
+
+selected_type = st.selectbox('Split for price distribution',list_for_hist)
+
+fig1 = px.histogram(df, x="model",color = selected_type )
+fig1.update_layout(title= "<b> Split of price by {}</b>".format(selected_type))
+
+st.plotly_chart(fig1)
+
+
+def age_category(x):
+    if x<5: return '<5'
+    elif  x>=5 and x<10: return '5-10'
+    elif x>=10 and x<20: return '10-20'
+    else: return '>20'
+df['age'] = 2024 - df['model_year']
+
+df['age_category'] = df['age'].apply(age_category)
+
+list_for_scatter = ['odometer','cylinders','paint_color']
+
+choice_for_scatter = st.selectbox('Price dependency on',list_for_scatter)
+
+fig2 = px.scatter(df, x="model", y=choice_for_scatter, color ="age_category",hover_data=['model_year'])
+fig2.update_layout(title="<b> Model vs {}</b>".format(choice_for_scatter))
+st.plotly_chart(fig2)
